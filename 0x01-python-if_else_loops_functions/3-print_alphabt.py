e90757073707846b466f51b247896c3c9190e9e4
Charles-Homks <charles01homks@gmail.com>
2021-11-16 12:29:39 -0800

    Printing ASCII characters for alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print("{}".format(chr(i)), end="")
+for n in range(97, 123):
+    if chr(n) != 'q' and chr(n) != 'e':
+        print("{}".format(chr(n)), end="")
