527218da3a73412c282fbfed7390785b3cd7774b
Charles-Homks <charles01homks@gmail.com>
2021-11-19 04:11:21 -0800

    Correction, variable for stderr

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for n in range(97, 123):
-    if chr(n) != 'q' and chr(n) != 'e':
-        print("{}".format(chr(n)), end="")
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end="")
